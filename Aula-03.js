32e3aa99e685ea01fd3339b97237d755001b078c
nicolas felipe <nfelipegouveia@outlook.com>
2025-11-28 01:59:04 -0300

    feat: Aula-03 Condicional if-else

---
+++ Aula-03.js
@@ -0,0 +1,11 @@
+//Condicional if-else
+const idade = 28;
+
+if(idade <= 28) {
+    console.log('ENTROU')
+} else {
+    console.log('NAO ENTRA!!!')
+}
+
+//Nesse exemplo se idade for menor do que 28 é escrito ENTROU no console
+//Se não for maior é escrito NÃO ENTRA
